650dfb43ad494671765e044355d96874164367e3
madrigal1 <kurupgokul11@gmail.com>
2020-04-16 22:34:11 +0530

    feat:Chat ui 2.0

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -6,14 +6,14 @@ import './Input.css';
 const Input = ({ setMessage, sendMessage, message }) => (
   <form className="form">
     <input
-      className="input"
+      className="input curvyBorder"
       type="text"
       placeholder="Type a message..."
       value={message}
       onChange={({ target: { value } }) => setMessage(value)}
       onKeyPress={(event) => (event.key === 'Enter' ? sendMessage(event) : null)}
     />
-    <button type="submit" className="sendButton" onClick={(e) => sendMessage(e)}>
+    <button type="submit" className="sendButton curvyBorder" onClick={(e) => sendMessage(e)}>
       Send
     </button>
   </form>
