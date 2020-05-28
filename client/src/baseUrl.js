565d79388209e4f4a48544e854083f566ac28cc5
madrigal1 <kurupgokul11@gmail.com>
2020-05-28 13:07:59 +0530

    feat:deploy

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,2 +1,2 @@
-/* export default 'https://agile-hamlet-78037.herokuapp.com'; */
-export default 'http://localhost:5000';
+export default 'https://agile-hamlet-78037.herokuapp.com';
+/* export default 'http://localhost:5000'; */
