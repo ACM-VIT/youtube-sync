683c3688bc1c1ecc3579e200abf3579943d7bb5c
madrigal1 <kurupgokul11@gmail.com>
2020-07-17 19:26:50 +0530

    feat:newBackend

--- server/models/dbInit.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.DB_URL || "mongodb://127.0.0.1:27017/ysDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log(`DB Connection successful`))
-  .catch((err) => console.error("dberror: ", err));
